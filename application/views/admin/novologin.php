e6e5f0910cc6dd0adc65c50dddcbe4648d2cceed
Raphaelcouto16 <50593539+Raphaelcouto16@users.noreply.github.com>
2022-10-03 22:36:56 -0300

    Tela de novo login

---
+++ application/views/admin/novologin.php
@@ -0,0 +1,50 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="title" content="<?php if (isset($seo_title)){echo $seo_title;}?>"/>
+        <meta name="description" content="<?php if (isset($seo_description)){echo $seo_description;}?>"/>
+        <meta name="Keywords" content="<?php if (isset($seo_keywords)){echo $seo_keywords;}?>" />
+        <meta name="language" content="pt-BR" />
+        <meta name="target_country" content="br" />
+        <meta name="rating" content="general"/>
+        <meta name="robots" content="index,follow"/>
+        <meta name="revisit-after" content="1 days"/>
+        <meta name="author" content="Felipe Govoni" />
+        <meta name="reply-to" content="contato@govoni.com.br"/>
+        <title><?php if (isset($seo_title)){echo $seo_title;}?></title>
+
+        <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css');?>"/>
+        <link href="<?php echo base_url('assets/fontawesome/css/all.css');?>" rel="stylesheet"/>
+        <link href="<?php echo base_url('assets/css/login.css');?>" rel="stylesheet"/>
+
+
+        <script src="<?php echo base_url('assets/js/jquery.js');?>"></script>
+        <script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.js');?>"></script>
+
+        
+        
+</head>
+<body class="text-center">
+ 
+<form class="form-signin" id="form_validate_login" action="<?php echo base_url(); ?>admin/login/incluilogin" method="post">
+  <img class="mb-4" src="" alt="" width="72" height="72">
+  <h1 class="h3 mb-3 font-weight-normal">Tela de cadastro</h1>
+  <label for="email" class="sr-only">E-mail</label>
+  <input type="email" id="email" class="form-control" placeholder="Email" required autofocus>
+  <label for="senha" class="sr-only">Senha</label>
+  <input type="password" id="senha" class="form-control" placeholder="Senha" required>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Criar</button>
+  <p class="mt-5 mb-3 text-muted">&copy; 2022</p>
+</form>
+
+</body>
+</html>
