d135fe134385402cfcc9932588d6a30675318d49
Breno Bolina <brenobolinareal@gmail.com>
2022-11-17 22:50:24 -0300

    17/11/2022

--- application/views/_template.php
+++ application/views/_template.php
@@ -32,142 +32,143 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
         <style>
             .topo{
-                background-color:red;
-                color:white;
+                background-color:#e5dadd;
+                color:black;
             }
 
             .topo a{
-                color:white;
+                color:black;
             }
 
             .nav-link{
-                color:white!important;
+                color:black!important;
             }
 
             .rodape{
-                background-color:red;
-                color:white;
+                background-color:#e5dadd;
+                color:black;
             }
 
             .rodape a{
-                color:white;
+                color:black;
             }
         </style>
         
 </head>
+
 <body>
 
-<div class='topo'>
-    <div class='container'>
-        <div class='row py-3'>
-            <div class='col-12 col-md-2 justify-content-center align-self-center'>
-                <img src='<?php echo base_url('assets/img/logo.png'); ?>' class='img-fluid'>
-            </div>
-            <div class='col-12 col-md-10 justify-content-center align-self-center'>
-                <nav class="navbar navbar-expand-lg navbar-light">
-
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('') ?>">Principal</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('quemsomos') ?>">Quem Somos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('blog') ?>">Blog</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('galeria') ?>">Galeria</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="<?php echo base_url('contato') ?>">Contato</a>
-                        </li>
-
-                        </ul>
-
-                    </div>
-                </nav>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-        <!--------------------- BANNER -------------------->
-<?php 
-    if(!empty($banners)){
-        if($this->uri->segment(1) == 'Principal' or $this->uri->segment(1) == ''){
-            echo "
-            <div id='carouselExampleControls' class='carousel slide' data-ride='carousel'>
-            <div class='carousel-inner'>";
-    
-            foreach($banners as $b){
-                //escrever o código para exibir os banners
-                echo "
-                <div class='carousel-item active'>
-                    <img class='d-block w-100' src='".base_url('assets/img/banner/'.$b->imagem)."' alt='First slide'>
+    <div class='topo'>
+        <div class='container'>
+            <div class='row py-3'>
+                <div class='col-12 col-md-2 justify-content-center align-self-center'>
+                    <img src='<?php echo base_url('assets/img/logo.png'); ?>' class='img-fluid'>
                 </div>
-                ";
-            }
-            echo "
-            </div>
-                <a class='carousel-control-prev' href='#carouselExampleControls' role='button' data-slide='prev'>
-                    <span class='carousel-control-prev-icon' aria-hidden='true'></span>
-                    <span class='sr-only'>Previous</span>
-                </a>
-                <a class='carousel-control-next' href='#carouselExampleControls' role='button' data-slide='next'>
-                    <span class='carousel-control-next-icon' aria-hidden='true'></span>
-                    <span class='sr-only'>Next</span>
-                </a>
-            </div>";
-        }
-    }
- ?>
-        
-        <!--------------------- BANNER -------------------->
+                <div class='col-12 col-md-10 justify-content-center align-self-center'>
+                    <nav class="navbar navbar-expand-lg navbar-light">
 
-<?php echo $contents;?>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo base_url('') ?>">Principal</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo base_url('quemsomos') ?>">Quem Somos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo base_url('blog') ?>">Blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo base_url('galeria') ?>">Galeria</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="<?php echo base_url('contato') ?>">Contato</a>
+                            </li>
 
-<div class='rodape'>
-    <div class='container'>
-        <div class='row'>
-            <div class='col-12'>
-                rodape
+                            </ul>
+
+                        </div>
+                    </nav>
+                </div>
             </div>
         </div>
     </div>
-</div>            
 
-<!--Cookie -->
-<div id="my-cookie-modal" class="cookie-modal">
-  <div class="cookie-modal-content">
-   <!--Caso queira colocar o titulo <div class="cookie-modal-header">
-      <p>Cookie Policy</p>
-    </div> -->
-    <div class="cookie-modal-body">
-      <p class="cookie-content m-0">
-      Este site usa cookies para melhorar sua experiência enquanto você navega pelo site. Os cookies são categorizados em “essenciais” e de “mídias”. Os cookies que são categorizados como “essenciais” são armazenados no seu navegador, pois são necessários para o funcionamento das principais funções do site. Estes cookies serão mantidos independentemente de seu consentimento.
-Também usamos cookies de “mídias” compartilhados com terceiros que nos ajudam a analisar e entender como você usa este site, possibilitando interações futuras com você. Estes cookies serão armazenados em seu navegador apenas com o seu consentimento e você tem a opção de cancelar esses cookies acessando os detalhes de nossa política. Contudo, importante informar que a desativação de alguns desses cookies poderá afetar sua experiência de navegação.”
-Detalhe sobre cookies essenciais: Os cookies “essenciais” são absolutamente necessários para o funcionamento adequado do site. Esta categoria inclui apenas cookies que garantem funcionalidades básicas e recursos de segurança do site. Esses cookies não armazenam nenhuma informação pessoal e serão utilizados independentemente de seu consentimento com respaldo legal.
-Detalhes sobre cookies de mídia: Quaisquer cookies que possam não ser particularmente necessários (“essenciais”) para o funcionamento do site e sejam usados especificamente para coletar dados pessoais do usuário por meio de análises, anúncios e outros conteúdos incorporados são denominados cookies de “mídia”. Esses cookies somente serão armazenados com o consentimento do usuário.
-      </p>
-      <span class="cookie-close">Prosseguir</span>
-    </div>
-    <div class="cookie-modal-footer">
-      <!--<span class="cookie-footer-item"><a target="_blank" href=""></a></span>-->
-      <span class="cookie-footer-item"><a target="_blank" href="https://goadopt.io/blog/cookies-e-lgpd/">Saiba mais</a></span>
 
+
+    <!--------------------- BANNER -------------------->
+    <?php 
+        if(!empty($banners)){
+            if($this->uri->segment(1) == 'Principal' or $this->uri->segment(1) == ''){
+                echo "
+                <div id='carouselExampleControls' class='carousel slide' data-ride='carousel'>
+                <div class='carousel-inner'>";
+        
+                foreach($banners as $b){
+                    //escrever o código para exibir os banners
+                    echo "
+                    <div class='carousel-item active'>
+                        <img class='d-block w-100' src='".base_url('assets/img/banner/'.$b->imagem)."' alt='First slide'>
+                    </div>
+                    ";
+                }
+                echo "
+                </div>
+                    <a class='carousel-control-prev' href='#carouselExampleControls' role='button' data-slide='prev'>
+                        <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+                        <span class='sr-only'>Previous</span>
+                    </a>
+                    <a class='carousel-control-next' href='#carouselExampleControls' role='button' data-slide='next'>
+                        <span class='carousel-control-next-icon' aria-hidden='true'></span>
+                        <span class='sr-only'>Next</span>
+                    </a>
+                </div>";
+            }
+        }
+    ?>
+        
+    <!--------------------- BANNER -------------------->
+
+    <?php echo $contents;?>
+
+    <!--------------------- RODAPE -------------------->
+    <div class='rodape'>
+        <div class='container'>
+            <div class='row'>
+                <div class="col-12 text-center">
+                    Desenvolvido por <a target='_blank' href="http://www.govoni.com.br">Govoni Soluções Digitais</a>
+                </div>
+            </div>
+        </div>
+    </div>            
+
+    <!--Cookie -->
+    <div id="my-cookie-modal" class="cookie-modal">
+    <div class="cookie-modal-content">
+    <!--Caso queira colocar o titulo <div class="cookie-modal-header">
+        <p>Cookie Policy</p>
+        </div> -->
+        <div class="cookie-modal-body">
+        <p class="cookie-content m-0">
+            Este site usa cookies para melhorar sua experiência enquanto você navega pelo site. Os cookies são categorizados em “essenciais” e de “mídias”. Os cookies que são categorizados como “essenciais” são armazenados no seu navegador, pois são necessários para o funcionamento das principais funções do site. Estes cookies serão mantidos independentemente de seu consentimento.
+            Também usamos cookies de “mídias” compartilhados com terceiros que nos ajudam a analisar e entender como você usa este site, possibilitando interações futuras com você. Estes cookies serão armazenados em seu navegador apenas com o seu consentimento e você tem a opção de cancelar esses cookies acessando os detalhes de nossa política. Contudo, importante informar que a desativação de alguns desses cookies poderá afetar sua experiência de navegação.”
+            Detalhe sobre cookies essenciais: Os cookies “essenciais” são absolutamente necessários para o funcionamento adequado do site. Esta categoria inclui apenas cookies que garantem funcionalidades básicas e recursos de segurança do site. Esses cookies não armazenam nenhuma informação pessoal e serão utilizados independentemente de seu consentimento com respaldo legal.
+            Detalhes sobre cookies de mídia: Quaisquer cookies que possam não ser particularmente necessários (“essenciais”) para o funcionamento do site e sejam usados especificamente para coletar dados pessoais do usuário por meio de análises, anúncios e outros conteúdos incorporados são denominados cookies de “mídia”. Esses cookies somente serão armazenados com o consentimento do usuário.
+        </p>
+        <span class="cookie-close">Prosseguir</span>
+        </div>
+        <div class="cookie-modal-footer">
+        <!--<span class="cookie-footer-item"><a target="_blank" href=""></a></span>-->
+        <span class="cookie-footer-item"><a target="_blank" href="https://goadopt.io/blog/cookies-e-lgpd/">Saiba mais</a></span>
+
+        </div>
     </div>
-  </div>
-</div>
-<!--Cookie -->
+    </div>
+    <!--Cookie -->
 
 </body>
 
--- application/views/principal.php
+++ application/views/principal.php
@@ -1,7 +1,83 @@
 <div class='container'>
-    <div class='row'>
-        <div class='col-12'>
-            principal
+
+    <div class='row px-3 my-5 d-flex'>
+        <div class='col-12 text-justify justify-content-center align-self-center'>
+            <h1 class='text-center'>Lorem Ipsum is simply dummy</h1>
         </div>
     </div>
+
+    <div class='row px-3 my-4 d-flex'>
+        <div class='col-12 col-md-7 text-justify justify-content-center align-self-center'>
+            <h1 class='text-center'>Lorem Ipsum is simply dummy</h1>
+            <br>
+            <p style='line-height: 1.6;'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
+            specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
+            unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+            with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum </p>
+        </div>
+
+        <div class='col-12 col-md-5'>
+            <div class="embed-responsive embed-responsive-4by3">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+
+    <div class='row px-3 my-4 d-flex'>
+
+        <div class='col-12 mt-5 text-center'>
+            <h2>Lorem Ipsum is simply dummy<h2>          
+        </div>
+
+        <div class='col-12 col-md-4 mt-4 py-3 text-justify'   id ="Servico" name="Servico">
+            <img src='<?php echo base_url('assets/img/') ?>' class='img-fluid mb-3'>
+            
+            <h5 class='text-center'><b>Lorem Ipsum is simply dummy</b></h5>
+             
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
+                unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+                with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 
+             
+            <a style="color: blue;" href='<?php echo base_url('psicoterapia') ?>' >Saiba mais</a>
+        </div>
+
+        <div class='col-12 col-md-4 mt-4 py-3 text-justify'  id ="Servico" name="Servico">     
+            <img src='<?php echo base_url('assets/img/') ?>' class='img-fluid mb-3'>
+
+            <h5 class='text-center'><b>Lorem Ipsum is simply dummy</b></h5>
+            
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
+                unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+                with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 
+        
+            <a style="color: blue;" href='<?php echo base_url('') ?>'>Saiba mais</a>   
+        </div>
+
+        <div class='col-12 col-md-4 mt-4 py-3 text-justify'  id ="Servico" name="Servico">
+            <img src='<?php echo base_url('assets/img/grupocalma.jpg') ?>' class='img-fluid mb-3'>
+            <h5 class='text-center'><b>Lorem Ipsum is simply dummy</b></h5>
+     
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
+                specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially 
+                unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+                with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum 
+         
+            <a style="color: blue;" href='<?php echo base_url('') ?>'>Saiba mais</a>  
+        </div>
+
+    </div>
+
+    <div class='row px-3 my-4 d-flex'>
+        <div class='col-12 text-center'>
+            <h2>Lorem Ipsum is simply dummy<h2>
+            <iframe width="560" height="315" src="https://www.youtube.com/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>       
+        </div>
+    </div>
+
 </div>
