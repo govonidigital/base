c5772ca143b3a88244702f65a858635d6ece335b
João Oliveira <86302934+JoaoOliveiraaa@users.noreply.github.com>
2024-07-05 10:49:13 -0300

    galeria edita_salva

--- application/controllers/admin/Galeria_fotos.php
+++ application/controllers/admin/Galeria_fotos.php
@@ -99,40 +99,51 @@ class Galeria_fotos extends CI_Controller {
         $this->template->load('admin/_template', 'admin/galeria_fotos_edita', $data);
     }
 
-	public function edita_salva(){
-
+	public function edita_salva() {
         $this->load->model('galeria_fotos_model');
         
-        $config['upload_path']          = './assets/img/galeria_fotos/';
-        $config['allowed_types']        = 'jpg|png|jpeg';
-        $config['encrypt_name']        = 'true';
-        $config['max_size']             = 2000;
-        $config['max_width']            = 2000;
-        $config['max_height']           = 1000;
-        
+        $config['upload_path'] = './assets/img/galeria/';
+        $config['allowed_types'] = 'JPG|PNG|JPEG|jpg|png|jpeg';
+        $config['encrypt_name'] = true;
+    
         $this->load->library('upload', $config);
-        
+    
         $data = array(
-        'titulo' => $this->input->post('titulo'), 
-        'link' => $this->input->post('link'),
-        'ordem' => $this->input->post('ordem'), 
-        'ativo' => $this->input->post('ativo'),
+            'titulo' => $this->input->post('titulo'), 
+            'id_galeria' => $this->input->post('id_galeria'), 
+            'link' => $this->input->post('link'),
+            'ordem' => $this->input->post('ordem'),
+            'ativo' => $this->input->post('ativo')
         );
-        
-        if ($this->upload->do_upload('imagem')){
-                $upload_data = $this->upload->data(); //Returns array of containing all of the data related to the file you uploaded.
-                $file_name = $upload_data['file_name'];
-                $data = $data + array('imagem' => $file_name);  
+    
+        // Check if a file is being uploaded
+        if ($this->upload->do_upload('imagem')) {
+            $upload_data = $this->upload->data(); // Returns array of containing all of the data related to the file you uploaded.
+            $file_name = $upload_data['file_name'];
+            $data['imagem'] = $file_name;
+    
+            $resize_config['image_library'] = 'gd2';
+            $resize_config['source_image'] = "./assets/img/galeria/$file_name";
+            $resize_config['maintain_ratio'] = TRUE;
+            $resize_config['width'] = 1600;
+    
+            $this->load->library('image_lib', $resize_config);
+            $this->image_lib->resize();
+        } else {
+            // If an image was uploaded, do nothing and continue
+            if (!empty($_FILES['imagem']['name'])) {
+                echo $this->upload->display_errors();
+                return;
             }
-            
-        
-        $retorno = $this->galeria_fotos_model->galeria_fotos_edita_salva($this->input->post('id_galeria_foto'),$data);
-        
-
- 
-
-        redirect('admin/galeria_fotos/lista/'.$this->input->post('id_galeria'),'refresh');
+        }
+    
+        // Save the updated data to the database
+        $id_galeria_foto = $this->input->post('id_galeria_foto');
+        $this->galeria_fotos_model->galeria_fotos_edita_salva($id_galeria_foto, $data);
+    
+        redirect('admin/galeria_fotos/lista/' . $this->input->post('id_galeria'), 'refresh');
     }
+    
 
 
 
